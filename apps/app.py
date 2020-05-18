bcfdced8db9332c0bdbe18d8953cd385fc2a2dab
winston.shih88 <winston.shih88@gmail.com>
2020-05-17 23:33:52 -0700

    challenge files

--- apps/app.py
+++ apps/app.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scraping
+import scraping_challenge
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config['MONGO_URI'] = 'mongodb://localhost27017/mars_app'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+#mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_app')
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
+   return render_template("index_challenge.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
+   mars_data = scraping_challenge.scrape_all()
    mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+   #print("Scraping Successful")
+   return redirect("/", code=302)
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
